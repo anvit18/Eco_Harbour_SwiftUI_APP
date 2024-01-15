a3626ae25d5bdc2de5d2d65b8f38f1d72841712b
Anvit pawar <av7720@srmist.edu.in>
2024-01-15 12:35:38 +0530

    Initial Commit

---
+++ Eco_Harbour_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Eco_Harbour_SwiftUI
+//
+//  Created by user1 on 15/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Eco_Harbour_SwiftUI/Eco_Harbour_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eco_Harbour_SwiftUIApp.swift
+//  Eco_Harbour_SwiftUI
+//
+//  Created by user1 on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Eco_Harbour_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
