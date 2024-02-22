0940a0594a0ee277970933e78560005632239ff6
anvit18 <av7720@srmist.edu.in>
2024-02-22 16:36:39 +0530

    record and history page functioning

---
+++ Eco_Harbour_SwiftUI/historyVIEW demo.swift
@@ -0,0 +1,18 @@
+//
+//  historyVIEW demo.swift
+//  Eco_Harbour_SwiftUI
+//
+//  Created by user1 on 22/02/24.
+//
+
+import SwiftUI
+
+struct historyVIEW_demo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    historyVIEW_demo()
+}
---
+++ Eco_Harbour_SwiftUI/recordDailyRenderCategory.swift
@@ -0,0 +1,18 @@
+//
+//  recordDailyRenderCategory.swift
+//  Eco_Harbour_SwiftUI
+//
+//  Created by user1 on 22/02/24.
+//
+
+import SwiftUI
+
+struct recordDailyRenderCategory: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    recordDailyRenderCategory()
+}
