2bb63897d7f64f75976f2490f430b190e5bd4184
anvit18 <av7720@srmist.edu.in>
2024-02-25 20:17:04 +0530

    added main carbon

---
+++ Eco_Harbour_SwiftUI/UserData.swift
@@ -0,0 +1,18 @@
+//
+//  UserData.swift
+//  Eco_Harbour_SwiftUI
+//
+//  Created by user1 on 25/02/24.
+//
+
+import SwiftUI
+
+struct UserData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserData()
+}
