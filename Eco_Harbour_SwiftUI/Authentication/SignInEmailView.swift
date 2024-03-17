ff09de84fd58a06e5ecdbd7c0904a91a786d4d2f
Vishal Tyagi <vishaltyagi.dev098@gmail.com>
2024-03-17 10:00:23 +0530

    Login test complete

---
+++ Eco_Harbour_SwiftUI/Authentication/SignInEmailView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInEmailView.swift
+//  Eco_Harbour_SwiftUI
+//
+//  Created by user1 on 17/03/24.
+//
+
+import SwiftUI
+
+struct SignInEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInEmailView()
+}
