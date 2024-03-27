037195650d0804b87a0f6e284eb1c88ff164e4f4
Vishal Tyagi <vishaltyagi.dev098@gmail.com>
2024-03-27 13:55:40 +0530

    Emission statistics test done

---
+++ Eco_Harbour_SwiftUI/History/Stats/Statistics.swift
@@ -0,0 +1,18 @@
+//
+//  Statistics.swift
+//  Eco_Harbour_SwiftUI
+//
+//  Created by user1 on 27/03/24.
+//
+
+import SwiftUI
+
+struct Statistics: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Statistics()
+}
